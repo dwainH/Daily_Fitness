c04df8e9faeed41fc69dd4fabc7eeae3b0af0295
syfqfrdaus <syafiqfirdaus3221@gmail.com>
2023-06-29 17:01:58 +0800

    Update register-action.php

--- register-action.php
+++ register-action.php
@@ -11,25 +11,34 @@ $weight = $_POST['weight'];
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-// Insert data into the user table
-$query = "INSERT INTO user (username, userpass, Name, Age, Gender, Weight, Height)
-          VALUES ('$username', '$password', '$name', '$age', '$gender', '$weight', '$height')";
+// Check if the username already exists in the database
+$checkQuery = "SELECT username FROM user WHERE username = '$username'";
+$checkResult = mysqli_query($connection, $checkQuery);
 
-if (mysqli_query($connection, $query)) {
-    echo '<script>alert("Registration successful!");</script>';
-
-    // Start a session
-    session_start();
-    
-    // Store user information in session variables
-    $_SESSION['username'] = $username;
-
-    header("Location: plan.php");
-    exit();
+if (mysqli_num_rows($checkResult) > 0) {
+    echo '<script>alert("User already registered. Please choose a different username.");</script>';
+    // Redirect the user to the registration page or display an appropriate message
 } else {
-    $error_message = mysqli_error($connection);
-    $error_code = mysqli_errno($connection);
-    echo '<script>alert("Registration failed. Error: ' . $error_message . ' (Error Code: ' . $error_code . ')");</script>';
+    // Insert data into the user table
+    $insertQuery = "INSERT INTO user (username, userpass, Name, Age, Gender, Weight, Height)
+                    VALUES ('$username', '$password', '$name', '$age', '$gender', '$weight', '$height')";
+
+    if (mysqli_query($connection, $insertQuery)) {
+        echo '<script>alert("Registration successful!");</script>';
+
+        // Start a session
+        session_start();
+        
+        // Store user information in session variables
+        $_SESSION['username'] = $username;
+
+        header("Location: plan.php");
+        exit();
+    } else {
+        $error_message = mysqli_error($connection);
+        $error_code = mysqli_errno($connection);
+        echo '<script>alert("Registration failed. Error: ' . $error_message . ' (Error Code: ' . $error_code . ')");</script>';
+    }
 }
 
 // Close the database connection
